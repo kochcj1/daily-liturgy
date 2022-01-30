ac55dcdf7a62036d3e824b353885597616de0336
Caleb Koch <kochcj1@calebs-air.myfiosgateway.com>
2022-01-29 21:58:10 -0500

    Menu for opening dailyliturgy.com outside of app

--- components/LiturgyNavigator.js
+++ components/LiturgyNavigator.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Linking } from 'react-native';
+import { IconButton, Menu } from 'react-native-paper';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -8,6 +10,12 @@ import ScriptureScreen from './ScriptureScreen';
 const Stack = createStackNavigator();
 
 export default function LiturgyNavigator() {
+  const [menuVisible, setMenuVisible] = useState(false);
+  const openWebsite = () => {
+    setMenuVisible(false);
+    Linking.openURL("https://dailyliturgy.com/");
+  };
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -22,6 +30,21 @@ export default function LiturgyNavigator() {
           component={HomeScreen}
           options={{
             title: "Friday, January 28, 2022",
+            headerRight: () => (
+              <Menu
+                visible={menuVisible}
+                onDismiss={() => setMenuVisible(false)}
+                anchor={
+                  <IconButton
+                    icon={require("../assets/icons8-menu-vertical-90.png")}
+                    color="white"
+                    onPress={() => setMenuVisible(true)}
+                  />
+                }
+              >
+                <Menu.Item onPress={openWebsite} title="More About Daily Liturgy" />
+              </Menu>
+            )
           }}
         />
         <Stack.Screen
